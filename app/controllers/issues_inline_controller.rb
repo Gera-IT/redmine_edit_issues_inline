2affa1a9573d5974e37e34bd140664ea5d2d86cf
Alex Sinelnikov <sinelniko@gmail.com>
2014-09-19 17:07:05 +0300

    fixed 403 error

--- app/controllers/issues_inline_controller.rb
+++ app/controllers/issues_inline_controller.rb
@@ -1,7 +1,21 @@
-class IssuesInlineController < IssuesController
+class IssuesInlineController < ApplicationController
 
+  before_filter :find_project_by_project_id
 
   def update_inline
+
+    @issue = Issue.find_by_id(params[:issues_inline_id])
+
+
+    respond_to do |format|
+      if @issue.update_attributes(params[:issue])
+        format.html { redirect_to(@issue, :notice => 'Issue was successfully updated.') }
+        format.json { respond_with_bip(@issue) }
+      else
+        format.html { render :action => "edit" }
+        format.json { respond_with_bip(@issue) }
+      end
+    end
 
   end
 

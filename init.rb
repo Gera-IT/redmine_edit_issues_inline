b23d686098f2fa0ca43a6ba05f9d9214c8823cc5
Alex Sinelnikov <sinelniko@gmail.com>
2014-10-28 15:46:18 +0200

    added credentials

--- init.rb
+++ init.rb
@@ -8,12 +8,12 @@ require_dependency 'issue_patch'
 # require_dependency 'inline_helper'
 
 Redmine::Plugin.register :redmine_edit_issues_inline do
-  name 'Task Inline Edit plugin'
+  name 'Redmine inline issues edit plugin'
   author 'Alex Sinelnikov'
   description 'Inline edit for fields on tasks list'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '1.0b'
+  url 'https://github.com/avdept/redmine_edit_issues_inline'
+  author_url 'https://github.com/avdept'
 
   project_module :inline_edit do
     permission :allow_inline_edit, :issues_inline => :update_inline

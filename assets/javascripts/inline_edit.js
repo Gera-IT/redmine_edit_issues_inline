81b97143ff1e88b11e219bf1c15f0c8841200533
Alex Sinelnikov <sinelniko@gmail.com>
2014-11-07 16:14:18 +0200

    made js more accurate

--- assets/javascripts/inline_edit.js
+++ assets/javascripts/inline_edit.js
@@ -29,12 +29,17 @@ $(document).ready(function() {
 
     $('.subject_link').click(function(e){
 
-        if ($(this).children().first().hasClass("update_issue_change_label") || $(this).children().first().hasClass("new_issue_change_label") )
-        {}
-        else if  ($(this).children().length > 0)
+        if ($(this).find('form.form_in_place').length != 0)
         {
             e.preventDefault();
         }
+
+//        if ($(this).children().first().hasClass("update_issue_change_label") || $(this).children().first().hasClass("new_issue_change_label") )
+//        {}
+//        else if  ($(this).children().length > 0)
+//        {
+//            e.preventDefault();
+//        }
     })
 });
 
